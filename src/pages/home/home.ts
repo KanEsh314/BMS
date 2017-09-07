a1499298caa4c96f66773a315101db6734a77e80
KanEsh314 <kanesh314@gmail.com>
2017-09-07 17:26:34 +0800

    First Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { StationPage } from '../station/station';
+import { TripPage } from '../trip/trip';
+import { ReportPage } from '../report/report';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +14,15 @@ export class HomePage {
 
   }
 
+  setStation(){
+  	this.navCtrl.push(StationPage);
+  }
+
+  createTrip(){
+  	this.navCtrl.push(TripPage);
+  }
+
+  todayReport(){
+  	this.navCtrl.push(ReportPage);
+  }
 }
